e0563fa68d400f25297f7b401c2ad02218adca57
Ian Munene <munene.ian@live.com>
2018-03-11 16:18:55 +0300

    all intent data from each list item being passed to second activity

--- app/src/main/java/com/codeschool/candycoded/DetailActivity.java
+++ app/src/main/java/com/codeschool/candycoded/DetailActivity.java
@@ -3,6 +3,7 @@ package com.codeschool.candycoded;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
@@ -14,12 +15,26 @@ public class DetailActivity extends AppCompatActivity {
 
         Intent intent = DetailActivity.this.getIntent();
         String candyName = "";
+        String candyImage = "";
+        String candyPrice = "";
+        String candyDesc = "";
 
-        if (intent != null && intent.hasExtra("candy_name")) {
+        if (intent != null && intent.hasExtra("candy_name"))
             candyName = intent.getStringExtra("candy_name");
-        }
 
-        TextView textView = (TextView) findViewById(R.id.tv_candy_detail);
+        if (intent != null && intent.hasExtra("candy_price"))
+            candyPrice = intent.getStringExtra("candy_name");
+
+        if (intent != null && intent.hasExtra("candy_image"))
+            candyImage = intent.getStringExtra("candy_image");
+
+        if (intent != null && intent.hasExtra("candy_price"))
+            candyPrice = intent.getStringExtra("candy_name");
+
+        TextView textView = findViewById(R.id.tv_candy_detail);
         textView.setText(candyName);
+
+        Log.d("DetailActivity", "Intent data: " + candyImage
+                + ", " + candyPrice + ", " + candyDesc);
     }
 }
--- app/src/main/java/com/codeschool/candycoded/MainActivity.java
+++ app/src/main/java/com/codeschool/candycoded/MainActivity.java
@@ -3,50 +3,43 @@ package com.codeschool.candycoded;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.TextHttpResponseHandler;
+
 import java.util.ArrayList;
 
+import cz.msebera.android.httpclient.Header;
+
 public class MainActivity extends AppCompatActivity {
+    private Candy[] candies;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        TextView textView = (TextView)this.findViewById(R.id.text_view_title);
+        TextView textView = this.findViewById(R.id.text_view_title);
         textView.setText(R.string.products_title);
 
-        final ArrayList<String> candy_list = new ArrayList<String>();
+        final ArrayList<String> candy_list = new ArrayList<>();
 
-        candy_list.add("Tropical Wave");
-        candy_list.add("Berry Bouncer");
-        candy_list.add("Grape Gummer");
-        candy_list.add("Apple of My Eye");
-        candy_list.add("Much Minty");
-        candy_list.add("So Fresh");
-        candy_list.add("Sassy Sandwich Cookie");
-        candy_list.add("Uni-pop");
-        candy_list.add("Strawberry Surprise");
-        candy_list.add("Wish Upon a Star");
-        candy_list.add("Planetary Pops");
-        candy_list.add("Watermelon Like Whoa");
-        candy_list.add("Twist 'n' Shout");
-        candy_list.add("Beary Squad Goals");
-        candy_list.add("ROYGBIV Spinner");
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(
                 this,
                 R.layout.list_item_candy,
                 R.id.text_view_candy,
                 candy_list
         );
 
-        ListView listView = (ListView)this.findViewById(R.id.list_view_candy);
+        ListView listView = this.findViewById(R.id.list_view_candy);
 
         listView.setAdapter(adapter);
 
@@ -55,10 +48,33 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Intent detailIntent = new Intent(MainActivity.this, DetailActivity.class);
-                detailIntent.putExtra("candy_name", candy_list.get(i));
+                detailIntent.putExtra("candy_name", candies[i].name);
+                detailIntent.putExtra("candy_image", candies[i].image);
+                detailIntent.putExtra("candy_price", candies[i].price);
+                detailIntent.putExtra("candy_description", candies[i].description);
                 startActivity(detailIntent);
 
             }
         });
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get("https://vast-brushlands-23089.herokuapp.com/main/api",
+                new TextHttpResponseHandler() {
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                        Log.e("AsyncHttpClient", "response = " + responseString);
+                    }
+
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, String responseString) {
+                        Log.d("AsyncHttpClient", "response = " + responseString);
+                        Gson gson = new GsonBuilder().create();
+                        candies = gson.fromJson(responseString, Candy[].class);
+                        adapter.clear(); // clears anything that's inside the adapter
+                        for (Candy candy : candies) {
+                            adapter.add(candy.name); // things are added to
+                        }
+                    }
+                });
     }
 }
